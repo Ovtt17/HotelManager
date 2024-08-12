a727f8aa4f18e16594b606c0af7f6d56cc9f480a
Ovett Mora <ovettmora17@gmail.com>
2024-08-11 22:20:02 -0600

    Add Room class with properties and data annotations

---
+++ Models/Room.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelManager.Models
+{
+    public class Room
+    {
+        [Key]
+        public int RoomId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public required string RoomType { get; set; }
+
+        [Required]
+        [DataType(DataType.Currency)]
+        public required decimal PricePerNight { get; set; }
+
+        // Navigation property for related reservations
+        [Required]
+        public ICollection<Reservation>? Reservations { get; set; }
+    }
+}
