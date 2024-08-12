0f9c67c5650f84eb6975bb8a1130b164e7ba0792
Ovett Mora <ovettmora17@gmail.com>
2024-08-11 22:10:47 -0600

    Add Guest class to Models.

---
+++ Models/Guest.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelManager.Models
+{
+    public class Guest
+    {
+        [Key]
+        public int GuestId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public required string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public required string Email { get; set; }
+
+        [Required]
+        [Phone]
+        public required string Phone { get; set; }
+
+        // Navigation property for related reservations
+        public ICollection<Reservation>? Reservations { get; set; }
+    }
+}
