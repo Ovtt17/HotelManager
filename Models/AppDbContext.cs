80f963667b15b74d6c27fe3e24736afa2e32ea99
Ovett Mora <ovettmora17@gmail.com>
2024-08-11 23:04:41 -0600

    Add AppDbContext to administrate database

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using HotelManager.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    public DbSet<Room> Rooms { get; set; }
+    public DbSet<Guest> Guests { get; set; }
+    public DbSet<Reservation> Reservations { get; set; }
+}
